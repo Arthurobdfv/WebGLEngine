9535041e163c6e2733dcbf01e39c07a3f04dc0a6
Arthuro Veríssimo <arthurobdfv@gmail.com>
2025-02-13 08:08:06 +0000

    Update matrix.mjs
    Added logic to reduce matrix to a new lower rank

--- matrix.mjs
+++ matrix.mjs
@@ -10,13 +10,16 @@ export class mat {
      *
      */    
     constructor(dimension) {
-       values = new Float32Array(dimension * dimension);
+       this.dimension = dimension;
+       this.values = new Float32Array(dimension * dimension);
        for(let i = 0; i< (dimension*dimension); i++){
-        values[i] = i % dimension == 0 ? 1 : 0;
+        this.values[i] = i % dimension == 0 ? 1 : 0;
        };
     }
 
-function asRedimention(newDimension){
-  return values.filter((x, idx) => (idx%newDimension) < newDimension);
+function asNewDimention(newDimension){
+if(newDimension < this.dimension){
+  return this.values.slice(0,newDimension*this.dimension).filter((val,idx) => (idx%this.dimension) < newDimension);
  }
+}
 }
