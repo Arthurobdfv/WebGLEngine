ef4d87c2d4d8375eec0c82bc8d0d59529f76320c
Arthuro Veríssimo <arthurobdfv@gmail.com>
2025-02-20 13:48:08 +0000

    Update matrix.mjs
    Adding support for projection matrix

--- matrix.mjs
+++ matrix.mjs
@@ -14,6 +14,7 @@ export class mat {
   t = new Float32Array(3);
   r = new Float32Array(3);
   s = new Float32Array([ 1, 1, 1,]);
+  fudge = 0;
 
   constructor(dimension) {
      this.dimension = dimension;
@@ -37,21 +38,29 @@ asNewDimention(newDimension){
     this.s = new Float32Array([x, y, z]);
   }
 
-  rotation(angle, angle2 = 0){
-    this.r = new Float32Array([angle, angle2]);
+  rotation(x = 0, y = 0 , z = 0 ){
+    this.r = new Float32Array([x, y, z]);
   }
 
   toMvp(){
     var copy = new Float32Array(this.values);
-    copy[0] = this.s[0] * Math.cos(deg2rad * this.r[0]);
-    copy[1] = -Math.sin(deg2rad * this.r[0]);
-    copy[this.dimension] = Math.sin(deg2rad * this.r[0]);
-    copy[this.dimension+1] = this.s[1]*Math.cos(deg2rad * this.r[0]);
-    copy[2*this.dimension+2] = this.s[2];
+    copy[0] = this.s[0] * Math.cos(deg2rad * this.r[1]) * Math.cos(deg2rad * this.r[2]);
+    copy[1] = Math.sin(deg2rad * this.r[2]);
+    copy[2] = -Math.sin(deg2rad * this.r[1]);
+    
+    copy[this.dimension] = -Math.sin(deg2rad*this.r[2])
+    copy[this.dimension+1] = this.s[1]*Math.cos(deg2rad * this.r[0]) * Math.cos(deg2rad*this.r[2]);
+    copy[this.dimension+2] = Math.sin(deg2rad * this.r[0]);
+    
+    copy[2*this.dimension] = Math.sin(deg2rad*this.r[1]);
+    copy[2*this.dimension+1] = -Math.sin(deg2rad*this.r[0]);
+    copy[2*this.dimension+2] = this.s[2] * Math.cos(deg2rad*this.r[0])* Math.cos(deg2rad*this.r[1]);
+    
+    copy[this.dimension*(this.dimension-1)-1] = this.fudge;
     copy[this.dimension*(this.dimension-1)] = this.t[0];
     copy[this.dimension*((this.dimension-1)+1)] = this.t[1];
     copy[this.dimension*((this.dimension-1)+2)] = this.t[2];
-    copy[this.dimension*((this.dimension-1)+2)] = 1;
+    copy[this.dimension*((this.dimension-1)+3)] = 1;
     return copy;
   }
 
