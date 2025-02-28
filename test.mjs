4cb3a9175f4af77dbc210a02bbc8034c93844353
Arthuro Veríssimo <arthurobdfv@gmail.com>
2025-02-28 09:19:25 +0000

    Update test.mjs

--- test.mjs
+++ test.mjs
@@ -7,6 +7,7 @@ precision highp float;
 uniform vec4 u_color;
 in vec3   v_normal;
 in vec3   v_pos; 
+in vec4   v_vertexColor;
  
 // we need to declare an output for the fragment shader
 out vec4 outColor;
@@ -17,7 +18,8 @@ void main() {
   float intensity = 0.3;
   vec3 inverseLightDir = normalize(lightPos-v_pos);
   float NdotL = dot(inverseLightDir, normalize(v_normal));
-  outColor = vec4(u_color.xyz * NdotL * intensity,1);
+  //outColor = vec4(u_color.xyz * NdotL * intensity,1);
+  outColor = v_vertexColor;
 }
 
 `
@@ -29,6 +31,7 @@ var vertexShaderSource = `#version 300 es
 precision highp float;
 in vec4 a_position;
 in vec4 a_normal;
+in vec4 a_vertexColor;
  
 // A matrix to transform the positions by
 uniform mat4 u_projMatrix;
@@ -37,6 +40,7 @@ uniform mat4 u_camMvp;
 
 out vec3 v_normal;
 out vec3 v_pos; 
+out vec4 v_vertexColor;
 // all shaders have a main function
 void main() {
   // Multiply the position by the matrix.
@@ -44,6 +48,7 @@ void main() {
   v_normal = (u_transform * a_normal).xyz;
   gl_Position = mvp * a_position;
   v_pos = gl_Position.xyz;
+  v_vertexColor = a_vertexColor;
 }
 `
 import * as aux from './glContext.mjs';
@@ -67,6 +72,7 @@ var program = aux.createProgram(context, vertexShader, fragShader);
 
 var positionAttributeLocation = context.getAttribLocation(program, "a_position");
 var normalAttributeLocation = context.getAttribLocation(program, "a_normal");
+var vertexColorAttributeLocation = context.getAttribLocation(program, "a_vertexColor");
 var uniform_CameraMVPLocation = context.getUniformLocation(program, "u_camMvp");
 var uniform_ProjMatLocation = context.getUniformLocation(program, "u_projMatrix");
 var uniform_ColorLocation = context.getUniformLocation(program, "u_color");
@@ -101,11 +107,14 @@ function setupCube(attrib, data, context){
   var offset = 0;
   var primitiveType = context.TRIANGLES;
   var count = 36;
-  context.vertexAttribPointer(positionAttributeLocation, size, type, normalize, 24, offset);
   context.enableVertexAttribArray(positionAttributeLocation);
+  context.vertexAttribPointer(positionAttributeLocation, size, type, normalize, 36, offset);
 
-  context.vertexAttribPointer(normalAttributeLocation, size, type, normalize, 24, 3*4);
   context.enableVertexAttribArray(normalAttributeLocation);
+  context.vertexAttribPointer(normalAttributeLocation, size, type, normalize, 36, 3*4);
+
+  context.enableVertexAttribArray(vertexColorAttributeLocation);
+  context.vertexAttribPointer(vertexColorAttributeLocation,size, type, normalize, 36, 6*4);
   
   objectsToDraw.push({attrib, offset:0,count, primitiveType})
 }
@@ -166,7 +175,7 @@ var s = [1, 1, 1];
 var mvp = new mat(4);
 var cameraMvp = new mat(4);
 cameraMvp.scale(0, 0,0);
-cameraMvp.position(1000, -1010);
+cameraMvp.position(300, -300);
 cameraMvp.rotation(0);
 cameraMvp.scale(1,1, 1);
 mvp.scale(1, 1);
@@ -184,24 +193,26 @@ function mainDraw(){
   context.clearColor(0,0,0,0);
   context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);
   context.useProgram(program);
-  objectsToDraw.forEach(element => {
-    context.bindVertexArray(element.attrib);
-    context.drawArrays(element.primitiveType, element.offset, element.count);
-  });
-  //context.uniform4f(uniform_ColorLocation, Math.sin(time++ * deg2rad), Math.sin(time++ *deg2rad + randOffset), 0.5, 1);
   context.uniform4f(uniform_ColorLocation,.7, .7, 0.3, 1);
   //t[0] = (Math.sin(time++ * deg2rad) + 1 / 2) * 50;
   //t[1] = (Math.cos(time++ * deg2rad) + 1 / 2) * 50;
   //t[0] = context.canvas.width/2;
   //t[1] = context.canvas.height/2;
-  r[0] = Math.sin(time++ * deg2rad);
-  r[1] = Math.cos(time++ * deg2rad);
+  var timeDeg2Rad = time++ * deg2rad;
+  text.innerHTML = timeDeg2Rad;
+  r[0] = Math.sin(timeDeg2Rad);
+  r[1] = Math.cos(timeDeg2Rad);
   t[2] = -600;
   t[1] = -100;
   mvp.position(t[0], t[1], t[2]);
   mvp.rotation(0,time++/3,0);
   var test = mvp.toMvp();
   context.uniformMatrix4fv(uniform_TransformLocation, false, mvp.toMvp());
+  //context.uniform4f(uniform_ColorLocation, Math.sin(time++ * deg2rad), Math.sin(time++ *deg2rad + randOffset), 0.5, 1);
+  objectsToDraw.forEach(element => {
+    context.bindVertexArray(element.attrib);
+    context.drawArrays(element.primitiveType, element.offset, element.count);
+  });
   requestAnimationFrame(mainDraw)
 }
 mainDraw();
