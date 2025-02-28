41c50750a7ef2b71898b6da4bc6fa3e34a16ea5b
Arthuro Veríssimo <arthurobdfv@gmail.com>
2025-02-28 13:12:28 +0000

    Update test.mjs

--- test.mjs
+++ test.mjs
@@ -8,18 +8,19 @@ uniform vec4 u_color;
 in vec3   v_normal;
 in vec3   v_pos; 
 in vec4   v_vertexColor;
+in vec3   v_lightPos;
  
 // we need to declare an output for the fragment shader
 out vec4 outColor;
  
 void main() {
   // Just set the output to a constant reddish-purple
-  vec3 lightPos = vec3(-100,200,-300);
   float intensity = 0.3;
-  vec3 inverseLightDir = normalize(lightPos-v_pos);
+  vec3 inverseLightDir = normalize(v_lightPos-v_pos);
   float NdotL = dot(inverseLightDir, normalize(v_normal));
   //outColor = vec4(u_color.xyz * NdotL * intensity,1);
   //outColor = v_vertexColor * NdotL;
+//outColor = vec4(vec3(NdotL * v_vertexColor.xyz),1);
 outColor = vec4(vec3(NdotL),1);
 }
 
@@ -38,10 +39,12 @@ in vec4 a_vertexColor;
 uniform mat4 u_projMatrix;
 uniform mat4 u_transform;
 uniform mat4 u_camMvp;
+uniform vec3 u_lightPos;
 
 out vec3 v_normal;
 out vec3 v_pos; 
 out vec4 v_vertexColor;
+out vec3 v_lightPos;
 // all shaders have a main function
 void main() {
   // Multiply the position by the matrix.
@@ -50,6 +53,7 @@ void main() {
   gl_Position = mvp * a_position;
   v_pos = gl_Position.xyz;
   v_vertexColor = a_vertexColor;
+  v_lightPos = u_lightPos;
 }
 `
 import * as aux from './glContext.mjs';
@@ -74,6 +78,7 @@ var program = aux.createProgram(context, vertexShader, fragShader);
 var positionAttributeLocation = context.getAttribLocation(program, "a_position");
 var normalAttributeLocation = context.getAttribLocation(program, "a_normal");
 var vertexColorAttributeLocation = context.getAttribLocation(program, "a_vertexColor");
+var uniform_LightPositionLocation = context.getUniformLocation(program, "u_lightPos");
 var uniform_CameraMVPLocation = context.getUniformLocation(program, "u_camMvp");
 var uniform_ProjMatLocation = context.getUniformLocation(program, "u_projMatrix");
 var uniform_ColorLocation = context.getUniformLocation(program, "u_color");
@@ -84,19 +89,32 @@ console.log(`Attrib location for a_position is ${positionAttributeLocation}`);
 
 
 var rectSize = [ 100, 100 ];
-var rectVerts = aux.getRectangle(0, 0, rectSize[0], rectSize[1], 100);
 
-var rectVerts2 = aux.getRectangle(-200,-200, 200, 150,150);
+var lightPos = [300, 150, -600];
+
+var rectVerts = aux.getRectangle(0, 0, rectSize[0], rectSize[1], 100);
+var rectVerts2 = aux.getRectangle(0,0, 200, 150,150);
+var lightVerts = aux.getRectangle(0,0,10,10,10);
+
 var vao = context.createVertexArray();
+var vaoTransform = new mat(4);
+vaoTransform.position(0,-250,-600);
 var vao2 = context.createVertexArray();
+var vao2Transform = new mat(4);
+vao2Transform.position(-200,-350, -600);
+var lightVao = context.createVertexArray();
+var lightTransform = new mat(4);
+
+
 
 
 
 var objectsToDraw = [];
-setupCube(vao, rectVerts, context);
-setupCube(vao2, rectVerts2, context);
+var cube1 = setupCube(vao, rectVerts, context, vaoTransform);
+var cube2 = setupCube(vao2, rectVerts2, context, vao2Transform);
+var light = setupCube(lightVao, lightVerts, context, lightTransform);
 
-function setupCube(attrib, data, context){
+function setupCube(attrib, data, context, objTransform){
   var positionBuffer = context.createBuffer();
   context.bindBuffer(context.ARRAY_BUFFER, positionBuffer);
   context.bufferData(context.ARRAY_BUFFER, new Float32Array(data), context.STATIC_DRAW);
@@ -117,7 +135,8 @@ function setupCube(attrib, data, context){
   context.enableVertexAttribArray(vertexColorAttributeLocation);
   context.vertexAttribPointer(vertexColorAttributeLocation,size, type, normalize, 36, 6*4);
   
-  objectsToDraw.push({attrib, offset:0,count, primitiveType})
+  objectsToDraw.push({attrib, offset:0,count, primitiveType, transform: objTransform})
+  return objectsToDraw.length-1;
 }
 context.viewport(0,0, context.canvas.width, context.canvas.height);
 
@@ -186,13 +205,14 @@ var test = cameraMvp.toMvp();
 var test2 = 1;
 function mainDraw(){
   if(resizeCanvasToDisplaySize(canvas)){
-   text.innerHTML = `Canvas size ${canvas.width}, ${canvas.height}`;
+    text.innerHTML = `Canvas size ${canvas.width}, ${canvas.height}`;
     context.viewport(0,0, canvas.width, canvas.height);
   } 
+  
+  context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);
   context.uniformMatrix4fv(uniform_ProjMatLocation, false, projectionMatrix.toMvp());
   context.uniformMatrix4fv(uniform_CameraMVPLocation, false, cameraMvp.inverse());
   context.clearColor(0,0,0,0);
-  context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);
   context.useProgram(program);
   context.uniform4f(uniform_ColorLocation,.7, .7, 0.3, 1);
   //t[0] = (Math.sin(time++ * deg2rad) + 1 / 2) * 50;
@@ -201,16 +221,20 @@ function mainDraw(){
   //t[1] = context.canvas.height/2;
   var timeDeg2Rad = time++ * deg2rad;
   text.innerHTML = timeDeg2Rad;
-  r[0] = Math.sin(timeDeg2Rad);
-  r[1] = Math.cos(timeDeg2Rad);
+  objectsToDraw[cube2].transform.scale(1, 1 + 0.2*Math.cos(timeDeg2Rad*10), 1);
+  objectsToDraw[cube1].transform.rotation(1,timeDeg2Rad*10 , 1);
+  objectsToDraw[light].transform.position(300, -150 + Math.cos(timeDeg2Rad) * 300, -600 + Math.sin(timeDeg2Rad)* 300);
+  var lightTransform = objectsToDraw[cube2].transform.getPos();
+  console.log(`Light pos is ${lightTransform[0]}, ${lightTransform[1]}, ${lightTransform[2]}`)
+  context.uniform3f(uniform_LightPositionLocation,false, lightTransform[0], lightTransform[1], lightTransform[2]);
   t[2] = -600;
   t[1] = -100;
   mvp.position(t[0], t[1], t[2]);
   mvp.rotation(0,time++/3,0);
   var test = mvp.toMvp();
-  context.uniformMatrix4fv(uniform_TransformLocation, false, mvp.toMvp());
   //context.uniform4f(uniform_ColorLocation, Math.sin(time++ * deg2rad), Math.sin(time++ *deg2rad + randOffset), 0.5, 1);
   objectsToDraw.forEach(element => {
+    context.uniformMatrix4fv(uniform_TransformLocation, false, element.transform.toMvp());
     context.bindVertexArray(element.attrib);
     context.drawArrays(element.primitiveType, element.offset, element.count);
   });
